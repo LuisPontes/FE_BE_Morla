e5310ea290d100cf0d3bf78f6814731cf218bb27
Luis Pontes <Luismarquespontes@gmail.com>
2018-07-26 12:15:24 +0100

    init

---
+++ ProjectMorla_FrontEnd_BackOffice/src/main/java/pt/morla/FrontEnd/ProjectMorlaFrontEndBackOfficeApplication.java
@@ -0,0 +1,12 @@
+package pt.morla.FrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMorlaFrontEndBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMorlaFrontEndBackOfficeApplication.class, args);
+	}
+}
