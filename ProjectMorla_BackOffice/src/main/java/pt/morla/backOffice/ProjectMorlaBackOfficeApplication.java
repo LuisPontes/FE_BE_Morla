135c1d524d6f9fe494fca76c97ca3a7efc1fee70
Luis Pontes <Luismarquespontes@gmail.com>
2018-07-26 12:41:34 +0100

    backoffice e frontend

---
+++ ProjectMorla_BackOffice/src/main/java/pt/morla/backOffice/ProjectMorlaBackOfficeApplication.java
@@ -0,0 +1,12 @@
+package pt.morla.backOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMorlaBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMorlaBackOfficeApplication.class, args);
+	}
+}
---
+++ ProjectMorla_FrontEnd/src/main/java/pt/morla/FrontEnd/ProjectMorlaFrontEndApplication.java
@@ -0,0 +1,12 @@
+package pt.morla.FrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMorlaFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMorlaFrontEndApplication.class, args);
+	}
+}
