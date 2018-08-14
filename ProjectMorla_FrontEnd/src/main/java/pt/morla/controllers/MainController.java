8653a988c8c092be121a8cccebde494b8e2c428e
Luis Pontes <Luismarquespontes@gmail.com>
2018-08-14 16:19:10 +0100

    dashboard v.1

--- ProjectMorla_FrontEnd/src/main/java/pt/morla/controllers/MainController.java
+++ ProjectMorla_FrontEnd/src/main/java/pt/morla/controllers/MainController.java
@@ -1,11 +1,15 @@
 package pt.morla.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -21,9 +25,14 @@ public class MainController {
 	
 	
 	@RequestMapping(value = { "/dashboard", "/db" }, method = { RequestMethod.GET, RequestMethod.POST })
-	public String dashboard(Model model) {
-
-
-		return "dashboad";
+	public String dashboard(HttpServletRequest request, HttpServletResponse response,ModelAndView model) {
+		
+		
+		
+		return "dashboard/index";
 	}
+	
+	
+	
+	
 }
