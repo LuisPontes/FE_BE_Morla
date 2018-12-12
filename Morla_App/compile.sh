30adde7a47d68c8684b1a508a302f97cd49582b6
Luis Pontes <luismarquespontes@gmail.com>
2018-12-12 11:12:40 +0000

    Beta v.0.5

---
+++ Morla_App/compile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ function compile() { 
+    echo "Compile ...[start]" 
+    #mvn clean compile assembly:single
+	mvn package
+    echo "Compile ...[OK]" 
+  }  
+ function startProject() { 
+    echo "Run Project..." 
+    java -jar target/MORLA.app-0.0.1-SNAPSHOT.jar
+  }  
+
+
+
+case "$1" in  
+  "c")
+		compile
+		;; 
+  "run")
+  		startProject
+		;; 
+
+  "all") 
+    	compile 
+    	startProject 
+  		;; 
+  *)	
+  		echo "Comandos:{ compile[c] | start project[run] | corre tudo (compile e upload)[all] }" 
+  		exit 1
+		;;
+esac
--- Morla_App/src/main/java/pt/morla/app/MorlaAppApplication.java
+++ Morla_App/src/main/java/pt/morla/app/MorlaAppApplication.java
@@ -2,10 +2,12 @@ package pt.morla.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages={"pt.morla.app.*"})
 @EnableJpaRepositories(basePackageClasses= {pt.morla.app.bo.db.interfaces.CategoriasRepository.class,pt.morla.app.bo.db.interfaces.ProjectosRepository.class})
+@EnableCaching
 public class MorlaAppApplication {
 
 	public static void main(String[] args) {
